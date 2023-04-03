2f3cee26c1f69448c3dfcae69b3fe41bfc531ee5
AlexeyN0vichihin <129694953+AlexeyN0vichihin@users.noreply.github.com>
2023-04-03 23:05:31 +0300

    Add files via upload

--- 1.R
+++ 1.R
@@ -16,9 +16,9 @@ sd(data$Education)
 model_1 = lm(Catholic~Examination, data)
 summary(model_1)
 #Построенная зависимость: Catholic(Examination)=90,51+(-3)*Examination
-#t-статистика показывает, что Catholic зависит от Examination, зависимость отрицательная
+#число *=3 показывает, что Catholic зависит от Examination, зависимость отрицательная
 #R-squared=0,31, показывает, что данная модель плохо предсказывает значение Catholic
 model_2 = lm(Catholic~Education, data)
 summary(model_2)
-#t-Статистика и R-squared, показывают на отсутствие линейной зависимости между Catholic и Education
+#число *=0 и R-squared=0,002, показывают на отсутствие линейной зависимости между Catholic и Education
 #Построенная зависимость: Catholic(Education)=48,47+(-0,67)*Education
