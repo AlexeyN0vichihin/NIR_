1b061504e4745250bc3bd30195830491453689a6
AlexeyN0vichihin <129694953+AlexeyN0vichihin@users.noreply.github.com>
2023-05-22 22:38:18 +0300

    Add files via upload

--- 3.R
+++ 3.R
@@ -1,3 +1,25 @@
 library("lmtest")
 data=read.csv("C:\\Users\\user\\Downloads\\r17i_os26b.csv",header= TRUE , stringsAsFactors= FALSE)
-mean(data$mid_h)
+df=data$mj13.2
+df
+df[df==99999997] <- 0
+df[df==99999998] <- 0
+df[df==99999999] <- 0
+df[is.na(df)] <- 0
+k=mean(df[df!=0])
+df[df==0]<-k
+data["df"]=df
+
+data$mi3
+df1=data$mi3
+df1[df1==99999997] <- 0
+df1[df1==99999998] <- 0
+df1[df1==99999999] <- 0
+df1[is.na(df1)] <- 0
+k1=mean(df1[df1!=0])
+df1[df1==0]<-k1
+data["df1"]=df1
+
+model_1=lm(df1~df,data)
+summary(model_1)
+plot(data$df,data$df1)
